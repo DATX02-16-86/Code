081e1ac8f624296a2be021cb05b712e575bc6f83
Rimmer <rimmer.motzheim@gmail.com>
2016-02-11 17:31:13 +0100

    Created default biome implementation

---
+++ Generator/Code/Pipeline/Biome/BiomeStage.cpp
@@ -0,0 +1,37 @@
+#include "BiomeStage.h"
+#include <vector>
+#include "../Pipeline.h"
+#include "../Height/HeightStage.h"
+
+namespace generator {
+
+
+void BiomeGenerator::generate(const Segment& segment, IdMatrix** auxiliaries, Pipeline& pipeline) {
+    auto map = pipeline.data.getOrCreate(Biomes, segment.detail);
+    generate(segment, map->getTile(segment.x, segment.y), auxiliaries, pipeline);
+}
+
+
+static std::vector<GenerateChunk> registeredBiomes;
+
+BiomeId registerBiome(GenerateChunk generateChunk) {
+    registeredBiomes.push_back(generateChunk);
+    return (BiomeId)registeredBiomes.size();
+}
+
+const BiomeId DefaultBiome::id = registerBiome(DefaultBiome::fillChunk);
+
+void DefaultBiome::fillChunk(Chunk& chunk, Pipeline& pipeline) {
+    auto height = pipeline.data.get(BaseHeight);
+    chunk.build([=](Voxel& current, Int x, Int y, Int z) -> Voxel {
+        Size baseHeight = 0;
+        if(height) baseHeight = height->get(x, y, z);
+
+        U16 blockType = 0;
+        if(z <= baseHeight) blockType = 1;
+
+        return Voxel {blockType};
+    });
+}
+
+}
---
+++ Generator/Code/Pipeline/Biome/BiomeStage.h
@@ -0,0 +1,60 @@
+
+#ifndef GENERATOR_CLIMATESTAGE_H
+#define GENERATOR_CLIMATESTAGE_H
+
+#include "../Generator.h"
+#include "../Voxel.h"
+
+namespace generator {
+
+struct Pipeline;
+
+GeneratorStream(Biomes, 8);
+
+/**
+ * Helper base class for biome id generators.
+ * This will automatically send the biome map to the overloaded generate().
+ */
+struct BiomeGenerator: Generator {
+    using Generator::Generator;
+
+    void generate(const Segment& segment, IdMatrix** auxiliaries, Pipeline& pipeline) override;
+
+    /**
+     * Generates biome id data for a specific sample segment.
+     * The default implementation just sets the default biome everywhere.
+     */
+    virtual void generate(const Segment& segment, IdMatrix& map, IdMatrix** auxiliaries, Pipeline& pipeline) {
+        segment.map([&](auto x, auto y) {
+            map.set(x, y, segment.detail, 0);
+        });
+    }
+};
+
+/// Currently, biome ids are represented as bytes. We may want to change this to 16-bit values
+/// in the future if it turns out that more than 256 biomes are used in one world.
+using BiomeId = U8;
+
+/// Chunk generators are called through this interface.
+using GenerateChunk = void(*)(Chunk&, Pipeline&);
+
+/**
+ * Registers a new Biome and returns its biome id.
+ * This must be done for all biomes that are used through the pipeline.
+ * If the returned biome id is stored in the biome stage,
+ * the pipeline will automatically call the corresponding biome in the terrain stage.
+ * @param fillChunk This is called to generate voxel data for this biome.
+ * The function should fill the provided chunk using the pipeline state.
+ */
+BiomeId registerBiome(GenerateChunk generateChunk);
+
+/// This is the global default biome that is used when no specific biome is defined or the requested biome doesn't exist.
+/// It creates a flat world at a low height.
+struct DefaultBiome {
+    static const BiomeId id;
+    static void fillChunk(Chunk& chunk, Pipeline& pipeline);
+};
+
+} // namespace generator
+
+#endif //GENERATOR_CLIMATESTAGE_H
