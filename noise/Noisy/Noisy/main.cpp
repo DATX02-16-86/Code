83eaf995289d6b965f8767f3a9d642413d904872
gabbbsson <gabrwagn@gmail.com>
2016-02-16 18:43:56 +0100

    Added our own simplex noise and update main to the latest verision which was removed in the GNU-purge. Requires Tritium submodule.

--- noise/Noisy/Noisy/main.cpp
+++ noise/Noisy/Noisy/main.cpp
@@ -1,42 +1,88 @@
 #include <iostream>
-#include "..\..\simplex\simplexnoise.h"
+#include <fstream>
+#include "..\simplex.h"
+
+const int chunk_width = 10;
+const int chunks_y = 4;
+const int chunks_x = 4;
+
+float point_z_values[chunk_width*chunks_y][chunk_width*chunks_x];
+
+int heights[chunks_x][chunks_y] = { { 1, 2, 1, 5 },{ 3, 6, 2, 4 },{ 2, 6, 3, 6 },{ 3, 0, 1, 2 } };
+//int heights[chunks_y][chunks_x] = { { 1, 2, 1 },{ 3, 6, 2 },{ 2, 6, 3 } };
+
+// heights in order: (0,0), (1,0), (1,1), (0,1)
+static float bilinearInterpolation(float x, float y, float* interpolationHeights)
+{
+	return interpolationHeights[0] * (1 - x) * (1 - y) + interpolationHeights[3] * x*(1 - y) + interpolationHeights[1] * (1 - x)*y + interpolationHeights[2] * x*y;
+}
+
+float calculate_height(int x, int y, int chunkX, int chunkY)
+{
+	float interpX = x + 0.5f; // shift x value to middle of voxel
+	float interpY = y + 0.5f; // shift y value to middle of voxel
+	float halfChunk = chunk_width / 2.0;
+
+	if (x < halfChunk) //left
+	{
+		interpX += halfChunk; // increase x coordinate by half a chunk
+		chunkX -= 1; // shift chunk coordinate to the left
+	}
+	else {
+		interpX -= halfChunk;
+	}
+	if (y < halfChunk) // top
+	{
+		interpY += halfChunk; // increase y coordinate by half a chunk
+		chunkY -= 1; // shift chunk coordinate to the top
+	}
+	else
+	{
+		interpY -= halfChunk;
+	}
+	float interpolationHeights[4] = { heights[chunkY][chunkX], heights[chunkY + 1][chunkX], heights[chunkY + 1][chunkX + 1], heights[chunkY][chunkX + 1] };
+
+	return bilinearInterpolation(interpX / chunk_width, interpY / chunk_width, interpolationHeights);
+}
 
 int main() {
 
-	int chunk_width = 5;
-	const int chunks_x = 1;
-	const int chunks_y = 3;
+	for (int chY = 0; chY < chunks_y; chY++) {
+		for (int j = 0; j < chunks_x; j++) {
+			std::cout << heights[chY][j];
+			std::cout << ",";
+		}
+		std::cout << std::endl;
+	}
+	std::cin.get();
+	for (int chY = 1; chY < chunks_y - 1; chY++) {
+		for (int chX = 1; chX < chunks_x - 1; chX++) {
+			int current_height = heights[chY][chX];
 
-	int heights[chunks_x][chunks_y] = { { 1, 2, 3 } };
-
-	for (int i = 0; i < chunks_x; i++)
-	{
-		for (int j = 0; j < chunks_y; j++)
-		{
-			int current_height = heights[i][j];
-
-			for (int x = 0; x < chunk_width; ++x)
-			{
-				for (int y = 0; y < chunk_width; ++y)
-				{
-					float z = octave_noise_2d(5, 0.25, 0.15, x + (chunk_width * i), y + (chunk_width * j)) + current_height;
-
-					std::cout << z;
-
-					if (y < chunk_width - 1)
-					{
-						std::cout << ",";
-					}
+			for (int y = 0; y < chunk_width; ++y) {
+				for (int x = 0; x < chunk_width; ++x) {
+					int true_x = x + (chunk_width * chX);
+					int true_y = y + (chunk_width * chY);
+					float z = simplex3D(true_x, true_y, 0) + calculate_height(x, y, chX, chY);
+					point_z_values[true_y][true_x] = z;
 				}
-
-				std::cout << std::endl;
 			}
 		}
+	}
+	// basic file operations
+	std::ofstream myfile;
+	myfile.open("points.txt");
 
+	for (int i = chunk_width; i < (chunks_x - 1)*chunk_width; ++i) {
+		for (int j = chunk_width; j < (chunks_y - 1)*chunk_width; ++j) {
+			myfile << point_z_values[i][j];
+			myfile << ",";
+		}
+
+		myfile << std::endl;
 	}
 
-
-	std::cin.get();
+	myfile.close();
 
 	return 0;
 }
