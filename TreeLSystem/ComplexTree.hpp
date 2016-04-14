c4bda4cf0ad48cc11342c238daa1bafd99498c04
lidin <lidin@student.chalmers.se>
2016-04-14 14:00:56 +0200

    Code for L-system.

---
+++ TreeLSystem/ComplexTree.hpp
@@ -0,0 +1,12 @@
+#ifndef ComplexTree_hpp
+#define ComplexTree_hpp
+
+#include <stdio.h>
+#include "TreeLSystem.hpp"
+
+struct ComplexTree: public TreeLSystem {
+  // Constructors.
+  ComplexTree();
+};
+
+#endif /* ComplexTree_hpp */
